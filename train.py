7f528c553a3bd7083fe749470bb4fdb16d731554
Robert Cowher <bobcowher@gmail.com>
2025-07-22 17:39:22 -0500

    updated to a max buffer of 200k

--- train.py
+++ train.py
@@ -10,9 +10,9 @@ batch_size = 64
 learning_rate = 0.0001
 epsilon = 1
 min_epsilon = 0.1
-epsilon_decay = 0.998
+epsilon_decay = 0.995
 gamma = 0.99
-max_buffer_size = 500000
+max_buffer_size = 200000
 
 hidden_layer = 512 
 
