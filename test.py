3e17fbf1584971cc97c75b644e613b720dc9c25b
Robert Cowher <bobcowher@gmail.com>
2025-08-08 07:54:20 -0500

    Updated to make the checkpoint model use more randomness

--- test.py
+++ test.py
@@ -1,45 +1,7 @@
-from agent import Agent
-import time
-from game import Pong
+from model import Model
 
-max_episode_steps = 10000
-total_steps = 0
-step_repeat = 4
-max_episode_steps = max_episode_steps / step_repeat
-
-batch_size = 64
-learning_rate = 0.0001
-epsilon = 1
-min_epsilon = 0.1
-epsilon_decay = 0.995
-gamma = 0.99
-
-hidden_layer = 512 
-
-# print(observation.shape)
-
-# Constants
-start_time = time.perf_counter()
-
-#env = Pong(player1="ai", player2="bot", render_mode="human")
-env = Pong(player1="bot", player2="ai", render_mode="human")
-
-summary_writer_suffix = f'dqn_lr={learning_rate}_hl={hidden_layer}_mse_loss_bs={batch_size}_double_dqn'
-
-agent = Agent(hidden_layer=hidden_layer,
-              learning_rate=learning_rate,
-              gamma=gamma,
-              max_buffer_size=1000,
-              env=env)
+model = Model(action_dim=3, hidden_dim=512, observation_shape=(3,84,84), obs_stack=3)
 
 
-# Training Phase 1
-agent.test()
 
-end_time = time.perf_counter()
-
-elapsed_time = end_time - start_time
-
-print(f"Elapsed time was: {elapsed_time}")
-    
 
